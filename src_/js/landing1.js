65a54086be28de63c08ecc939f39090ba78ddf01
yoojunsu <vcoavmv@naver.com>
2022-08-30 11:13:49 +0900

    [PC] 랜딩페이지 리뷰 swiper, 라운지 이미지 심플리 스크롤 적용

--- src_/js/landing1.js
+++ src_/js/landing1.js
@@ -1,3 +1,24 @@
-$(function(){
-    
+$(function () {
+
+    //리뷰 스와이퍼
+    const review = new Swiper("#reviewSwiper", {
+        slidesPerView: "auto",
+        //slidesPerGroup: 3,
+        spaceBetween: 10,
+        loop: true,
+        loopedSlides: 10000,
+        centeredSlides: true,
+        speed: 400,
+        grabCursor: true,
+        navigation: {
+            nextEl: "#reviewSwiper .swiper-button-next",
+            prevEl: "#reviewSwiper .swiper-button-prev",
+        },
+    });
+
+    // 라운드 이미지 flow
+    $("#loungeList").simplyScroll({
+        //pauseOnHover: true,
+        speed: 1,
+    });
 });
